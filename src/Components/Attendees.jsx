b00a111d45facea30b9dc961f79fce729ab70512
Jeffchua96 <jeffersonchua@pursuit.org>
2023-08-03 19:02:15 -0400

    1st commit

--- src/Components/Attendees.jsx
+++ src/Components/Attendees.jsx
@@ -1,3 +1,29 @@
-export default function Attendees() {
-  return;
-}
+import Attendee from "./Attendee";
+import { useState } from "react"
+
+export default function Attendees({event, attendees, updateEventAttendance}) {
+  const [showAttendees, setAttendees] =useState(false)
+
+  function toggleEventsAttendees() {
+    setShowAttendees( !showAttendees );
+  }
+  return(
+    <>
+                      <button onClick={toggleEventAttendees}>
+                        {!showAttendees ? "Show Attendees" : "Hide Attendees"}
+                      </button>
+
+                      {showAttendees ? (
+                        <div className="attendees">
+                          {attendees.map((attendee, index) => (
+                            
+                            <Attendee 
+          attendee={attendee} 
+          updateEventAttendance={updateEventAttendance} 
+          index={index}
+          />
+                          ))}
+                        </div>
+                      ) : null}
+                    </>
+  )}
--- src/Components/Event.jsx
+++ src/Components/Event.jsx
@@ -1,3 +1,24 @@
-export default function Event() {
-  return;
+import Attendees from "./Attendees";
+
+export default function Event({event, showAttendees, attendees, updateAttendance}) {
+  return(
+    <div>
+                  <li key={event.id}>
+                    <img src={event.eventImage} alt={event.name} />
+                    <h5>
+                      {event.name} {event.eventType}
+                    </h5>
+                    <br />
+                    <span>Organized by: {event.organizer} </span>
+                    <br />
+                    <Attendees
+                    event={event}
+                    showAttendees= {showAttendees}
+                    attendees= {attendees}
+                    updateAttendance= {updateEventAttendance}
+                    />
+                  </li>
+                  </div>
+  )
 }
+                      
